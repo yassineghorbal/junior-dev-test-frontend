dac3f28f2e7530c2dbbe751159663899e5b38530
yassineghorbal <yassineghorbal2000@gmail.com>
2022-09-07 20:49:24 +0100

    changed aws endpoint urls

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -15,7 +15,7 @@ export default class ProductList extends React.Component {
 
     // get products using axios
     componentDidMount() {
-        axios.get("//ec2-3-89-196-32.compute-1.amazonaws.com/dashboard/api/read.php")
+        axios.get("https://ec2-3-89-196-32.compute-1.amazonaws.com/dashboard/api/read.php")
             .then(res => {
                 const products = res.data.data;
                 this.setState({ products });
@@ -39,13 +39,13 @@ export default class ProductList extends React.Component {
         let ids = this.state.ids
         if (ids.length > 0) {
             for (let i = 0; i < ids.length; i++) {
-                axios.delete("//ec2-3-89-196-32.compute-1.amazonaws.com/dashboard/api/delete.php", {
+                axios.delete("https://ec2-3-89-196-32.compute-1.amazonaws.com/dashboard/api/delete.php", {
                     data: {
                         "id": ids[i]
                     }
                 }).catch(error => console.log(error))
             }
-            axios.get("//ec2-3-89-196-32.compute-1.amazonaws.com/dashboard/api/read.php")
+            axios.get("https://ec2-3-89-196-32.compute-1.amazonaws.com/dashboard/api/read.php")
                 .then(res => {
                     const products = res.data.data;
                     this.setState({ products });
