2ed46cd7cbe0387897293a6b9667fae0f4213406
yassineghorbal <yassineghorbal2000@gmail.com>
2022-09-07 21:13:52 +0100

    changed back to 000webhost endpoints

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,19 +3,19 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = app => {
     app.use(
         createProxyMiddleware("/create.php", {
-            target: "https://junior-dev-test-api.000webhostapp.com/api/",
+            target: "https://junior-dev-test-api.000webhostapp.com/api",
             changeOrigin: true
         })
     )
     app.use(
         createProxyMiddleware("/read.php", {
-            target: "https://junior-dev-test-api.000webhostapp.com/api/",
+            target: "https://junior-dev-test-api.000webhostapp.com/api",
             changeOrigin: true
         })
     )
     app.use(
         createProxyMiddleware("/delete.php", {
-            target: "https://junior-dev-test-api.000webhostapp.com/api/",
+            target: "https://junior-dev-test-api.000webhostapp.com/api",
             changeOrigin: true
         })
     )
